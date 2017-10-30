68baeda4d940b2c4e0af9ac963691ae8f5231f78
Dr. Volker Göbbels <vmg@arachnion.de>
2017-10-30 15:24:33 +0100

    Added a complete ELK stack (thanks to Will Durand!) and added apache logs to that

--- Dockerfile
+++ Dockerfile
@@ -3,3 +3,5 @@ FROM wordpress:php7.1
 RUN curl -o /usr/local/bin/wp -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && chmod a+x /usr/local/bin/wp
 
 COPY kickoff.sh .
+
+RUN  rm -f /var/log/apache2/access.log && rm -f /var/log/apache2/error.log && rm -f /var/log/apache2/other_vhosts_access.log
--- kickoff.sh
+++ kickoff.sh
@@ -1,6 +1,7 @@
 wp core install --allow-root --url=localhost --title="My new Wordpress" --admin_user=admin --admin_password=adminpw --admin_email=test@test.com
 wp --allow-root plugin uninstall akismet
+wp --allow-root plugin uninstall hello
+wp --allow-root plugin install wordpress-importer --activate
 wp --allow-root plugin update --all
-wp --allow-root theme install flat --activate 
 wp --allow-root theme update --all
 wp --allow-root language core update
